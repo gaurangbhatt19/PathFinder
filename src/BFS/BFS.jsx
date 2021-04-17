c61ba855c6b2fb594ec1fedf92eab24482666b2e
GBSourceCodes <gaurangbhatt134@gmail.com>
2021-04-17 17:49:49 +0530

    BFS

---
+++ src/BFS/BFS.jsx
@@ -0,0 +1,18 @@
+import React, { Component} from 'react';
+import ReactDOM from 'react-dom';
+import './BFS.css';
+
+export default class BFS extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+        <div className="BFS">
+            <canvas></canvas>
+        </div>
+        );
+    }
+}
+
+
